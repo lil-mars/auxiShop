53cfd55afbe00e7431be5f47549d76b1c5913885
x-lil-peep-x <ermateze@gmail.com>
2020-07-10 22:30:18 -0400

    Done - Adding most sold spare chart Doing - Adding more charts if there are money

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\SaleDetail;
 use App\Models\Spare;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -26,10 +28,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $spare_count = Spare::all()->count();
-        $client_count = Client::all()->count();
-        $user_count = User::all()->count();
+        $spare_count = Spare::count();
+        $client_count = Client::count();
+        $user_count = User::count();
 
-        return view('admin.home', compact(['spare_count', 'client_count', 'user_count']));
+        $spares = DB::table('sale_details')
+            ->select(
+                DB::raw('sum(sale_details.quantity) as SumQuantities, sale_details.spare_id,spares.description')
+            )
+            ->join('spares', 'sale_details.spare_id', '=', 'spares.id')
+            ->groupBy('sale_details.spare_id')
+            ->orderBy('SumQuantities', 'desc')->get();
+        $list = array_slice($spares->all(), 0, 5);
+        return view('admin.home', compact(['spare_count', 'client_count', 'user_count', 'list']));
     }
 }
