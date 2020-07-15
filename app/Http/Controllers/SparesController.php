c44c550b0ad6801f8bb1fd4f6e33018ad1490f86
x-lil-peep-x <ermateze@gmail.com>
2020-07-15 15:02:04 -0400

    Done - Fixing image lenght 255 to 1000

--- app/Http/Controllers/SparesController.php
+++ app/Http/Controllers/SparesController.php
@@ -182,7 +182,7 @@ class SparesController extends Controller
             'price' => 'required|numeric|min:-999999.99|max:999999.99',
             'price_m' => 'required|numeric|min:-999999.99|max:999999.99',
             'investment' => 'required|numeric|min:-999999.99|max:999999.99',
-            'image' => 'required|min:0|max:255',
+            'image' => 'required|min:0|max:1000',
         ];
 
         $data = $this->validate($request, $rules);
